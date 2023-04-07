65b70d454ad548dba5a17a3b13cf2e923a2daba2
taneka <112857258+MrTaneka@users.noreply.github.com>
2023-04-07 18:20:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,6 +13,6 @@ def solution(p: float, x: np.array) -> tuple:
     n = x.shape[0]
     left1 = chi2.ppf(1 - alpha / 2, 2 * n)
     right1 = chi2.ppf(alpha / 2, 2 * n)
-    left = np.sqrt(sum(x ** 2) / (left1 * 35))
-    right = np.sqrt(sum(x ** 2) / (right1 * 35))
+    left = np.sqrt(sum(x ** 2) / (left1 * 23))
+    right = np.sqrt(sum(x ** 2) / (right1 * 23))
     return left, right
